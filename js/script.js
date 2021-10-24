68517adc8d06411e24c981ea07cbbb0ad39ac0fa
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2021-10-24 14:25:43 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-horizontal-scroll","displayName":"Horizontal scroll","link":{"linkType":"LinkTypePage","href":"#!page-horizontal-scroll"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
-$(window).load(function () {
-    $(".trigger_popup_fricc").click(function(){
-       $('.hover_bkgr_fricc').show();
-    });
-    $('.hover_bkgr_fricc').click(function(){
-        $('.hover_bkgr_fricc').hide();
-    });
-    $('.popupCloseButton').click(function(){
-        $('.hover_bkgr_fricc').hide();
-    });
-});
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
